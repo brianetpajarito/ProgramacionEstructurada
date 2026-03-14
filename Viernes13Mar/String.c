b922731f6f2fc1587ec351f95326b9276140f4a0
brianetpajarito <brianet.pajarito1903@alumnos.udg.mx>
2026-03-13 22:23:06 -0600

    Update String.c

--- Viernes13Mar/String.c
+++ Viernes13Mar/String.c
@@ -6,21 +6,21 @@ int main(){
     char Alumno2[16];
 
     //SCANF
-    printf("¿Cual es tu nombre? ");
+    printf("Cual es tu nombre? ");
     scanf("%s",&Alumno2); //%s porque es un string
     printf("(scanf) Tu nombre es: %s", Alumno2);
 
     //FGETS
-    printf("\n\n¿Cual es tu nombre y apellido? ");
+    /*printf("\nCual es tu nombre y apellido? ");
     fgets(Alumno2,sizeof(Alumno2),stdin); //fgets( DondeLoGuardo, sizeof(Tamaño), DondeLeo)
-    printf("\n(fgets) Tu nombre es: %s", Alumno2);
+    printf("(fgets) Tu nombre es: %s", Alumno2);*/
 
     //LISTA
     char lista [5][16]; //[filas][columnas]
 
-    printf("\nLISTA\n");
+    printf("\n\nLISTA\n");
     for(int i = 0; i < 3; i++) { //Pide 3 nombres
-        printf("¿Cual es tu nombre? ");
+        printf("Cual es tu nombre? ");
         fgets(lista[i],sizeof(lista[i]),stdin); //lo guarda en la lista, donde i es la fila
     }
     printf("\n");
@@ -36,6 +36,48 @@ int main(){
     for(int i = 0; i < 3; i++) { //imprime las primeras 3 filas de la lista
         longitud=strlen(lista[i]); //Strlen=longitud
         printf("\nLa longitud de %s es de %d", lista[i],longitud);
+    }
+
+    //STRCPY
+    printf("\n\nCOPIA DE LISTA\n");
+    char copia[16];
+
+    for(int i = 0; i < 3; i++) { //imprime las primeras 3 filas de la lista
+        strcpy(copia,lista[i]); //Strlen=longitud
+        printf("%s", copia);
+    }
+    printf("\n");
+
+    //STRCAT
+    printf("\nCONCATENACION\n");
+    char nombre[16];
+    char apellido[16];
+
+    printf("Cual es tu nombre? ");
+    scanf("%s",&nombre);
+    printf("¿Cual es tu apellido? ");
+    scanf("%s",&apellido);
+
+    strcat(nombre, " ");
+    strcat(nombre, apellido);
+    printf("Tu nombre completo es: %s", nombre);
+
+    //STRCMP
+    printf("\n\nCOMPARACION\n");
+
+    char nombre2[16];
+
+    printf("Cual es tu nombre? ");
+    scanf("%s",&nombre);
+    printf("Confirma tu nombre ");
+    scanf("%s",&nombre2);
+
+    strcmp(nombre, nombre2);
+    if(strcmp(nombre,nombre2) == 0){
+        printf("Los nombres SI coinciden");
+    }
+    else if(strcmp(nombre,nombre2)!=0){
+        printf("Los nombres NO coinciden");
     }
 
     return 0;
